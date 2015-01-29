a4063c37014144ce8a074065a7996b7388528b1a
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-01-29 18:20:39 +0000

    Mostly correct sneeze mechanics

--- SneezeFly/SneezeFly/GameScene.swift
+++ SneezeFly/SneezeFly/GameScene.swift
@@ -9,32 +9,71 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let playerSprite = SKSpriteNode(imageNamed: "Spaceship")
+    var mouseDownLocation: CGPoint?
+    
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
+        myLabel.text = "Sneeze Fly";
         myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame) * 1.5);
         self.addChild(myLabel)
+        
+        // Initialise player sprite
+        playerSprite.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame) * 0.5)
+        playerSprite.setScale(0.5)
+        
+        // Add physics body to sprite
+        playerSprite.physicsBody            = SKPhysicsBody(circleOfRadius: playerSprite.size.width/2.0)
+        playerSprite.physicsBody?.dynamic   = true
+        
+        self.addChild(playerSprite)
+        
+        // Add physics environment
+        self.physicsWorld.gravity = CGVectorMake(0.0, -3.0)
+        
+        sneeze()
+        
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+    
+    func sneeze() {
+        playerSprite.physicsBody?.velocity.dy = 0
+        
+        let sneezeMagnitude: CGFloat = 500.0
+        let sneezeVector             = CGVectorMake(sneezeMagnitude * -sin(playerSprite.zRotation),
+                                                    sneezeMagnitude * +cos(playerSprite.zRotation))
+        playerSprite.physicsBody?.applyImpulse(sneezeVector)
     }
     
     override func mouseDown(theEvent: NSEvent) {
         /* Called when a mouse click occurs */
         
-        let location = theEvent.locationInNode(self)
+        mouseDownLocation = theEvent.locationInNode(self)
         
-        let sprite = SKSpriteNode(imageNamed:"Spaceship")
-        sprite.position = location;
-        sprite.setScale(0.5)
         
-        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-        sprite.runAction(SKAction.repeatActionForever(action))
+        //        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+        //        sprite.runAction(SKAction.repeatActionForever(action))
         
-        self.addChild(sprite)
     }
     
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    override func mouseDragged(theEvent: NSEvent) {
+        println("dragged")
+        if let startLocation = mouseDownLocation {
+            let shipDirectionVector = CGVectorMake( theEvent.locationInNode(self).x - startLocation.x,
+                                                    theEvent.locationInNode(self).y - startLocation.y)
+            let myAngle: CGFloat = CGFloat(M_PI) - CGFloat(atan2(shipDirectionVector.dx, shipDirectionVector.dy))
+            playerSprite.zRotation = myAngle
+        }
+    }
+    
+    override func mouseUp(theEvent: NSEvent) {
+        sneeze()
+        mouseDownLocation = nil
     }
 }
